34c96686aaff388ed26dbf6fd874607013d62b2f
vsnu1995 <svishnu.1995@gmail.com>
2018-04-27 21:31:39 +0530

    Added signin feature

--- config/passport.js
+++ config/passport.js
@@ -16,15 +16,15 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function (req, email, password, done) {
-    req.checkBody('email','Invalid email').notEmpty().isEmail();
-    req.checkBody('password','Invalid password').notEmpty().isLength({min:4});
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 4 });
     var errors = req.validationErrors();
-    if(errors){
+    if (errors) {
         var messages = [];
-        errors.forEach(function(error){
+        errors.forEach(function (error) {
             messages.push(error.msg);
         });
-        return done(null,false,req.flash('error',messages));
+        return done(null, false, req.flash('error', messages));
     }
     User.findOne({ 'email': email }, function (err, user) {
         if (err) {
@@ -45,4 +45,34 @@ passport.use('local.signup', new LocalStrategy({
         });
     })
 
+}));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function (req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 4 });
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+
+    User.findOne({ 'email': email }, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, { message: 'No user found' });
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, user);
+    })
 }));
