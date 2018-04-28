78873beb98b3c4182394824128575f5e557e759c
vsnu1995 <svishnu.1995@gmail.com>
2018-04-28 09:11:46 +0530

    updated npm modules, added cart session

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
-
+var MongoStore = require('connect-mongo')(session);
 mongoose.connect('mongodb://localhost:27017/shopping');
 require('./config/passport');
 // view engine setup
-app.engine('.hbs',expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
@@ -27,28 +27,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'mysupersecret',resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'mysupersecret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
-app.use('/user',userRoutes);
+app.use('/user', userRoutes);
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   Product.find(function (err, result) {
     res.render('shop/index', { title: 'Shopping Cart', products: result });
   });
 });
+router.get('/add-to-cart/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart.items : {});
 
+  Product.findById(productId, function (err, product) {
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/');
+  });
+});
 module.exports = router;
